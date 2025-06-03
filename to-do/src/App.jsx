2cf9c8b2bc31616381217c5fe0d98b61d6aeb920
mukti2216patel <mukti2216patel@gmail.com>
2025-06-04 00:58:57 +0530

    null

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import {TodoProvider} from './contexts'
+import {TodoProvider} from './context'
 import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
--- to-do/src/components/TodoForm.jsx
+++ to-do/src/components/TodoForm.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
-import { useTodo } from '../contexts/TodoContext';
+import { useTodo } from '../context/TodoContext';
 
 function TodoForm() {
     const [todo, setTodo] = useState("")
     const {addTodo} = useTodo()
 
     const add = (e) => {
-      e.preventDefault()
+      e.preventDefault();
 
-      if (!todo) return
+      if (!todo) return;
 
-      addTodo({ todo, completed: false})
-      setTodo("")
+      addTodo({ todo, completed: false});
+      setTodo("");
     }
 
   return (
