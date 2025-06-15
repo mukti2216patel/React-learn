bc09a21fe1fe1593bae289c9d60b55ef17c1b34d
mukti2216patel <mukti2216patel@gmail.com>
2025-06-15 20:57:40 +0530

    null

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [loading , setLoading] = useState(true);
   console.log(import.meta.env.REACT_APP_APPWRITE_URL);
   return (
     <>
